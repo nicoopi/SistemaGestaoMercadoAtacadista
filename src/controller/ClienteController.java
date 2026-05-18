06310d88cfce4bf3cf3ad238b007f4279d6eadbf
alestrechar <130070050+alestrechar@users.noreply.github.com>
2026-05-18 15:34:01 -0300

    Criando classes: ClienteController, ClienteRepository, MercadoView e a interface Descontavel. Somente para estruturação do projeto.

---
+++ src/controller/ClienteController.java
@@ -0,0 +1,5 @@
+package controller;
+
+public class ClienteController {
+    // Somente criação da classe
+}
---
+++ src/model/Descontavel.java
@@ -0,0 +1,5 @@
+package model;
+
+public interface Descontavel {
+    // Somente criação da interface
+}
