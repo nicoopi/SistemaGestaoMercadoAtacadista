0e13419a90f2740936a9fdbdcfc5a9a9981d1494
Nicolas Oliveira Antunes da Silva <nicolasoliveiradasilva2107@gmail.com>
2026-05-19 19:56:21 -0300

    Criação das assinaturas dos métodos - SalvarDados() e LerDados() da Classe ArquivoUtil, para usar posteriormente nos Controllers.

--- src/util/ArquivoUtil.java
+++ src/util/ArquivoUtil.java
@@ -1,4 +1,13 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArquivoUtil {
+    public void salvarDados(String nomeDoArquivo, List<String> listaParaSalvar) {
+    }
+
+    public List<String> lerDados(String nomeArquivo) {
+        return new ArrayList<>();
+    }
 }
