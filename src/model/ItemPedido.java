aeba2e8f884877c298871d8273897d96c37c3837
Nicolas Oliveira Antunes da Silva <nicolasoliveiradasilva2107@gmail.com>
2026-05-21 20:39:21 -0300

    Criação da Classe ItemPedido com seus devidos atributos, construtor, getters & setters, e os métodos da classe.

--- src/model/ItemPedido.java
+++ src/model/ItemPedido.java
@@ -1,4 +1,48 @@
 package model;
 
 public class ItemPedido {
+    private Produto produto;
+    private int quantidadePedida;
+    private double precoUnitario;
+
+    public ItemPedido (Produto produto, int quantidadePedida) throws IllegalArgumentException{
+        if(produto == null) {
+            throw new IllegalArgumentException("ERRO: O produto não pode ser nulo! Insira um produto válido.");
+        }
+
+        this.produto = produto;
+        setQuantidadePedida(quantidadePedida);
+        setPrecoUnitario(produto.getPrecoBase());
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidadePedida() {
+        return quantidadePedida;
+    }
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setQuantidadePedida(int quantidadePedida) throws IllegalArgumentException {
+        if(quantidadePedida <= 0) {
+            throw new IllegalArgumentException("ERRO: A quantidade deve ser maior que zero! Coloque uma quantidade válida.");
+        }
+        this.quantidadePedida = quantidadePedida;
+    }
+
+
+    public void setPrecoUnitario(double precoUnitario) throws IllegalArgumentException {
+        if(precoUnitario <= 0) {
+            throw new IllegalArgumentException("ERRO: O preço unitário do produto deve ser maior que zero!");
+        }
+        this.precoUnitario = precoUnitario;
+    }
+
+    public double calcularSubTotal() {
+        return getQuantidadePedida() * getPrecoUnitario();
+    }
+
 }
