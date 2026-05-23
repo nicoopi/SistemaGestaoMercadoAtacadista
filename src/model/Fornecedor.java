f650bed10fd63068d98c8d55b6fcbd96f5fa1c0c
Lucas <lucasteche170405@gmail.com>
2026-05-23 13:12:40 -0300

    Criacao das classes Fornecedor e OrdemCompra, juntamente de seus atributos e metodos

---
+++ src/model/Fornecedor.java
@@ -0,0 +1,70 @@
+package model;
+
+public class Fornecedor {
+    private String razaoSocial;
+    private String cnpj;
+    private String telefone;
+
+    public Fornecedor( String razaoSocial, String cnpj, String telefone){
+        setRazaoSocial(razaoSocial);
+        setCnpj(cnpj);
+        setTelefone(telefone);
+
+    }
+
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+
+    public void setRazaoSocial(String razaoSocial) throws IllegalArgumentException{
+        if (razaoSocial == null || razaoSocial.isBlank()){
+            throw new IllegalArgumentException("ERRO: A razão social não pode ser NULA ou VAZIA");
+        }
+        this.razaoSocial = razaoSocial;
+    }
+
+
+    public void setCnpj(String cnpj) throws IllegalArgumentException{
+        if(cnpj == null || cnpj.isBlank()) {
+            throw new IllegalArgumentException("ERRO: O CNPJ é um campo obrigatório. O valor recebido não pode ser nulo ou vazio.");
+        }
+
+        cnpj = cnpj.toUpperCase();
+        if(!cnpj.matches("[0-9A-Z]{12}[0-9]{2}")) {
+            throw new IllegalArgumentException("ERRO: CNPJ inválido! O valor recebido não pode ter pontuação ou símbolos.");
+        }
+
+        this.cnpj = cnpj;
+    }
+
+
+    public void setTelefone(String telefone) throws IllegalArgumentException{
+        if(telefone == null || telefone.isBlank()) {
+            throw new IllegalArgumentException("ERRO: O telefone é um campo obrigatório. O valor recebido não pode ser nulo ou vazio.");
+        }
+        if(!telefone.matches("[0-9()+\\- ]{11,15}")) {
+            throw new IllegalArgumentException("ERRO: O telefone ultrapassou a quantidade permitida de caracteres ou está sem DDD. Digite um telefone válido!");
+        }
+
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Razão Social: " + getRazaoSocial() + " | CNPJ: " + getCnpj() + " | Telefone: " + getTelefone();
+    }
+
+
+}
+
+
---
+++ src/model/OrdemCompra.java
@@ -0,0 +1,93 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrdemCompra {
+    private Fornecedor fornecedor;
+    private List<Produto> produtos;
+    private int quantidade;
+    private double custoUnitario;
+
+
+    public OrdemCompra(Fornecedor fornecedor, int quantidade, double custoUnitario) throws IllegalArgumentException {
+        if (fornecedor == null) {
+            throw new IllegalArgumentException("ERRO: O fornecedor não pode ser nulo! Insira um fornecedor válido.");
+        }
+
+        this.fornecedor = fornecedor;
+        this.produtos = new ArrayList<>();
+        setQuantidade(quantidade);
+        setCustoUnitario(custoUnitario);
+    }
+
+    public void adicionarProduto(Produto produto) throws IllegalArgumentException{
+        if (produto == null) {
+            throw new IllegalArgumentException("ERRO: O produto não pode ser nulo! Insira um produto válido.");
+        }
+        this.produtos.add(produto);
+    }
+
+    public void removerProduto(Produto produto) throws IllegalArgumentException {
+        if (produto == null) {
+            throw new IllegalArgumentException("ERRO: O produto não pode ser nulo! Insira um produto válido.");
+        }
+        boolean encontrado = false;
+        for (Produto p : produtos) {
+            if (p == produto) {
+                encontrado = true;
+                break;
+            }
+        }
+        if (!encontrado) {
+            throw new IllegalArgumentException("ERRO: Produto não encontrado!");
+        }
+        this.produtos.remove(produto);
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getCustoUnitario() {
+        return custoUnitario;
+    }
+
+    public void setQuantidade(int quantidade) throws IllegalArgumentException{
+        if (quantidade <= 0) {
+            throw new IllegalArgumentException("ERRO: A quantidade deve ser maior que zero! Coloque uma quantidade válida.");
+        }
+        this.quantidade = quantidade;
+    }
+
+    public void setCustoUnitario(double custoUnitario)throws IllegalArgumentException{
+        if (custoUnitario <= 0){
+            throw new IllegalArgumentException("ERRO: O custo unitário deve ser maior que zero! Coloque um custo válido.");
+        }
+        this.custoUnitario = custoUnitario;
+    }
+
+    public double calcularTotal(){
+        return getCustoUnitario() * getQuantidade();
+    }
+
+    @Override
+    public String toString() {
+        String ordemCompra = "Fornecedor: " + fornecedor.getRazaoSocial()
+                + " | Quantidade: " + getQuantidade()
+                + " | Custo Unitário: R$ " + getCustoUnitario()
+                + " | Custo Total: R$ " + calcularTotal() + "\n";
+        for (Produto p : produtos) {
+            ordemCompra += p.toString() + "\n";
+        }
+        return ordemCompra;
+    }
+}
