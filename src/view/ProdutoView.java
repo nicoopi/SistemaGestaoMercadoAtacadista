c8e85651d374083aca26ffe0c278c9a48c4d6652
Luiz Eduardo de Carvalho Novaki <luiz.novaki07@gmail.com>
2026-05-24 19:17:02 -0300

    Criação do ProdutoView e seus respectivos metodos, lerNomeProduto lerPrecoProduto lerIDProtudo exibirProduto exibirMensagem lerOpcaoProduto limparBuffer MostrarMenuproduto fecharScanner

--- src/view/ProdutoView.java
+++ src/view/ProdutoView.java
@@ -1,4 +1,56 @@
 package view;
 
+import model.Produto;
+import java.util.Scanner;
+
+
 public class ProdutoView {
+     private Scanner sc = new Scanner(System.in);
+
+        public void mostrarMenuProduto(){
+            System.out.println("---MENU PRODUTO---");
+            System.out.println("1 - CADASTRAR PRODUTO ");
+            System.out.println("2 - EXIBIR PRODUTO");
+            System.out.println("3 - SAIR");
+        }
+
+        public void limparBuffer(){
+            sc.nextLine();
+        }
+        public int lerOpcaoProduto(){
+            System.out.print("Qual é a opção?");
+            return sc.nextInt();
+        }
+        public void exibirMensagem(String mensagem){
+            System.out.println(mensagem);
+        }
+        public void fecharScanner() {
+            sc.close();
+        }
+
+        public String lerNomeProduto(){
+            System.out.print("Digite um nome válido para o produto: ");
+            return sc.nextLine();
+        }
+
+        public double lerPrecoProduto() {
+            System.out.print("Digite o preço base do produto: ");
+            return sc.nextDouble();
+        }
+
+        public int lerIDProduto() {
+            System.out.print("Digite um ID válido para o produto: ");
+            return sc.nextInt();
+        }
+
+        public void exibirProduto(Produto produto) {
+            if (produto == null) {
+                System.out.println("PRODUTO VAZIO");
+            } else {
+                System.out.println("-----INFORMAÇÕES DO PRODUTO-----");
+                System.out.println(produto.toString());
+
+            }
+        }
+
 }
