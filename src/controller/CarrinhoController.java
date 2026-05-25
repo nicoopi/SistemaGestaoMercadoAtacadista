e003e9cd175de1ca7df2e74406351a9b21f39e7e
Nicolas Oliveira Antunes da Silva <nicolasoliveiradasilva2107@gmail.com>
2026-05-24 23:30:09 -0300

    riação do CarrinhoView e CarrinhoController com lógica de adicionar produtos e exibir carrinho. Fiz mais uma alteração no .gitignore para não ocasionar mais problemas.

--- src/controller/CarrinhoController.java
+++ src/controller/CarrinhoController.java
@@ -1,4 +1,46 @@
 package controller;
 
+import model.Cliente;
+import model.Estoque;
+import model.ItemPedido;
+import model.Produto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarrinhoController {
+    private List<ItemPedido> itensCarrinho;
+    private Cliente clienteAtual;
+    private Estoque estoque;
+
+    public CarrinhoController(Estoque estoque) throws IllegalArgumentException{
+        if(estoque == null) {
+            throw new IllegalArgumentException("ERRO: O estoque não pode ser nulo. O carrinho precisa de conexão com o estoque para funcionar!");
+        }
+
+        this.estoque = estoque;
+
+        this.itensCarrinho = new ArrayList<>();
+        this.clienteAtual = null;
+    }
+
+    public void adicionarProdutonoCarrinho(Produto produto, int quantidadeDesejada)  throws IllegalArgumentException{
+        if(produto == null) {
+            throw new IllegalArgumentException("ERRO: O produto selecionado é inválido.");
+        }
+        if (quantidadeDesejada <= 0) {
+            throw new IllegalArgumentException("ERRO: A quantidade deve ser maior que zero.");
+        }
+
+        if(quantidadeDesejada <= estoque.getQuantidadeAtual()) {
+            ItemPedido novoItem = new ItemPedido(produto, quantidadeDesejada);
+            this.itensCarrinho.add(novoItem);
+        } else {
+            throw new IllegalArgumentException("ERRO: Estoque insuficiente! Não temos " + quantidadeDesejada + " unidades na prateleira.");
+        }
+    }
+
+    public List<ItemPedido> getItensCarrinho() {
+        return this.itensCarrinho;
+    }
 }
--- src/view/CarrinhoView.java
+++ src/view/CarrinhoView.java
@@ -1,4 +1,54 @@
 package view;
 
+import model.ItemPedido;
+import java.util.List;
+import java.util.Scanner;
+
 public class CarrinhoView {
+    private Scanner sc = new Scanner(System.in);
+
+    public void mostrarMenuCarrinho() {
+        System.out.println("=== MENU CARRINHO ===");
+        System.out.println("1 - Adicionar produto ao carrinho.");
+        System.out.println("2 - Exibir itens do carrinho.");
+        System.out.println("3 - Sair (ou Voltar).");
+    }
+
+
+    public int lerOpcaoCarrinho() {
+        System.out.println("Digite a opção desejada: ");
+        return sc.nextInt();
+    }
+
+    public int lerIDProduto() {
+        System.out.print("Digite o ID do Produto que deseja: ");
+        return sc.nextInt();
+    }
+
+    public int lerQuantidadeDesejada() {
+        System.out.print("Digite a Quantidade Desejada: ");
+        return sc.nextInt();
+    }
+
+    public void exibirCarrinho(List<ItemPedido> listaItens) {
+        if( listaItens == null || listaItens.isEmpty()) {
+            System.out.println("--- O CARRINHO ESTÁ VAZIO ---");
+        } else {
+            for(ItemPedido item : listaItens) {
+                System.out.println(item);
+            }
+        }
+    }
+
+    public void exibirMensagem(String msg) {
+        System.out.println(msg);
+    }
+
+    public void limparBuffer() {
+        sc.nextLine();
+    }
+
+    public void fecharScanner() {
+        sc.close();
+    }
 }
